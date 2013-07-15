329814f183078b03e0f87496a4e4bac7f69c0623
Wei Nie <kanzakiv@gmail.com>
2013-07-15 05:01:40 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var buffer = new Buffer(16);
+buffer = fs.readFileSync(index.html);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Wei!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
