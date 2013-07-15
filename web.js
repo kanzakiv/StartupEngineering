b16f6242db20953268079d86251264c7b7b57dc8
Wei Nie <kanzakiv@gmail.com>
2013-07-15 05:06:06 +0000

    updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
